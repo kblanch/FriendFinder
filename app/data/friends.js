1c04476452d32a0cbdb9c22c3d54a53c1f2af44a
Karen <K.Blanchette07@gmail.com>
2017-08-13 23:09:34 -0500

    HTML files setup, apiRoutes completed

---
+++ app/data/friends.js
@@ -0,0 +1,9 @@
+var friendsArray = [
+    {
+        name: "Ahmed",
+        photo: "link",
+        scores: [5,1,4,4,5,1,2,5,4,1]
+    }
+];
+
+module.exports = friendsArray;
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -2,13 +2,49 @@ var friendsData = require("../data/friends");
 
 module.exports = function(app){
 
-    app.get("/api/tables", function(req, res){
+    app.get("/api/friends", function(req, res){
         res.json(friendsData);
     });
 
-    app.post("/api/tables", function(req,res){
+    app.post("/api/friends", function(req,res){
         //API Data
         //Compatibility LOGIC
+        var newUser = 0;
+        var oldUser = 0;
+
+        var comparisonsArray = [];
+
+
+
+        req.body.scores.forEach(function(element) {
+            newUser += +element;
+        });
+
+        for(var i=0; i<friendsData.length;i++){
+            for( var j=0; j<friendsData[i].scores[j].length; j++){
+                oldUser += +friendsData[i].scores[j];
+            }
+            comparisonsArray.push(Math.abs(newUser - oldUser));
+        }
+
+        var leastDiff = comparisonsArray[0];
+        var leastDiffIndex = 0;
+
+        for(var k=1; k<comparisonsArray.length; k++){
+            if(comparisonsArray[k]<leastDiff){
+                leastDiff = k;
+            }
+        }
+
+        console.log(comparisonsArray);
+        console.log(leastDiff);
+        console.log(leastDiffIndex);
+        console.log(friendsData[leastDiffIndex]);
+        
+        friendsData.push(req.body);
+
+        console.log(friendsData);
+        res.json(friendsData[leastDiffIndex]);
     });
 
 };
